34686be0e432f0035146c99e4808390a4c22c15b
matias erenchun <matiaserenchun@gmail.com>
2020-10-31 23:02:17 -0300

    mejoras a dice roll

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,29 +3,48 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.ActivityChooserView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var diceImage : ImageView
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
         var rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice() }
         var countUp:Button = findViewById(R.id.Count_Up)
-        countUp.setOnClickListener{ countUp()}
+        //countUp.setOnClickListener{ countUp()}
 
     }
 
     private fun rollDice()
     {
+
+        //val diceImage: ImageView = findViewById(R.id.dice_image)
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt)
+        {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
        //Toast.makeText(this, "button clicked",Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-        val randomInt = (1..100).random()
+        /*val resultText: TextView = findViewById(R.id.result_text)
+
         resultText.text = randomInt.toString()
+         */
     }
 
+    /*
     private fun countUp()
     {
         val texto: TextView = findViewById(R.id.result_text)
@@ -34,4 +53,5 @@ class MainActivity : AppCompatActivity() {
         newInt++
         texto.text=newInt.toString()
     }
+     */
 }
